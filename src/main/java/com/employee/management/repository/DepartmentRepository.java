9f5959a799af8e54a1132aa6677a085348b972ec
Nitin <nitin_gakhar@epam.com>
2023-06-01 23:35:26 -0400

    Changes for Employee management REST api using Spring Data JPA.

---
+++ src/main/java/com/employee/management/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package com.employee.management.repository;
+
+import com.employee.management.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Department getDepartmentByDeptName(String deptHead);
+}
